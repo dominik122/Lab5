0f94514ee1a38fc39e0c2bf96176b123ffebc45e
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-30 15:32:26 +0200

    Proszuanie się Kulki

--- src/pl/lublin/wsei/java/cwiczenia/lab5/Pong.java
+++ src/pl/lublin/wsei/java/cwiczenia/lab5/Pong.java
@@ -1,5 +1,7 @@
 package pl.lublin.wsei.java.cwiczenia.lab5;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -7,6 +9,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Pong extends Application {
 
@@ -21,24 +24,35 @@ public class Pong extends Application {
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
 
+    private double x = ARENAX1 + ARENAWIDTH/2;
+
     @Override
     public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
-
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1+ARENAWIDTH/2, ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
+        t.play();
 
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void run(GraphicsContext gc){
+        gc.setFill(Color.BLACK);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(x += 5, ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
+
     }
 }
