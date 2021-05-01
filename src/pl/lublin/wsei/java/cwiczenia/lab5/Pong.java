e5bd1cd11dcdc2076cb1de67f85f658e2338b623
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-05-01 10:27:37 +0200

    Wersja z wieloma kulkami

--- src/pl/lublin/wsei/java/cwiczenia/lab5/Pong.java
+++ src/pl/lublin/wsei/java/cwiczenia/lab5/Pong.java
@@ -11,6 +11,8 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.Random;
+
 public class Pong extends Application {
 
     private static final double HEIGHT = 600;
@@ -24,12 +26,20 @@ public class Pong extends Application {
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
 
-    private double x = ARENAX1 + ARENAWIDTH/2;
+
+    private static final int LICZBAKULEK = 10;
+    private double[] x = new double[LICZBAKULEK];
+    private double[] y = new double[LICZBAKULEK];
+    private double[] vx = new double[LICZBAKULEK];
+    private double[] vy = new double[LICZBAKULEK];
+
+
 
     @Override
     public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        initKula();
 
         Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
         t.setCycleCount(Timeline.INDEFINITE);
@@ -47,12 +57,34 @@ public class Pong extends Application {
         launch(args);
     }
 
+    private void initKula(){
+        Random lott = new Random();
+        for (int i = 0; i < LICZBAKULEK; i++){
+            x[i] = lott.nextDouble() * ARENAWIDTH + ARENAX1;
+            y[i] = lott.nextDouble() * ARENAHEIGHT + ARENAY1;
+            vx[i] = 5+lott.nextDouble()*20;
+            vy[i] = 5+lott.nextDouble()*20;
+        }
+    }
+
     private void run(GraphicsContext gc){
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(x += 5, ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
 
+        for (int i = 0; i < LICZBAKULEK; i++){
+            if ((x[i] <= ARENAX1) || (x[i] >= ARENAX2)) vx[i] = -vx[i];
+            if ((y[i] <= ARENAY1) || (y[i] >= ARENAY2)) vy[i] = -vy[i];
+        }
+
+        for (int i = 0; i < LICZBAKULEK; i++){
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+
+        for (int i = 0; i < LICZBAKULEK; i++){
+            gc.setFill(Color.WHITESMOKE);
+            gc.fillOval(x[i] - R, y[i] - R, 2*R, 2*R);
+        }
     }
 }
